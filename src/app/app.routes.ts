420f499a25e0b0919c5432c242894bf5d1f4d0d1
Kjeff24 <jeffreyarthur123@gmail.com>
2025-01-01 18:08:32 +0000

    handle redirect with code from cognito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { AuthComponent } from './components/auth/auth.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
         title: 'Task Management | Home',
-    },
-    {
-        path: 'code',
-        component: AuthComponent,
-        title: 'Task Management | Authorized',
     },
 ];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { TokenService } from '../../services/token/token.service';
+import { environment } from '../../../environments/environment';
+import { HttpParams } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TokenResponse } from '../../models/token-reponse';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +13,43 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  code = '';
+
+  constructor(
+    private tokenService: TokenService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((data) => {
+      this.code = data['code'];
+      if(this.code) {
+        this.getToken(this.code);
+      }
+    });
+  }
+
+  login() : void {
+    let params = new HttpParams()
+      .set('response_type', environment.response_type)
+      .set('client_id', environment.client_id)
+      .set('redirect_uri', environment.redirect_uri)
+
+    location.href = environment.login_endpoint + '?' + params
+  }
+  
+  getToken(code: string): void {
+    this.tokenService.getToken(this.code).subscribe({
+      next: (data: TokenResponse) => {
+        this.tokenService.setTokens(data.id_token)
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    })
+  }
+
 
 }
