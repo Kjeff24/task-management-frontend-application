9d743455445f02913569b29157f54ffff729850b
Kjeff24 <jeffreyarthur123@gmail.com>
2025-01-08 12:09:10 +0000

    change return type from TaskResponse to Task

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    user_pool_domain_uri: '',
+    token_endpoint: '',
+    login_endpoint: '',
+    grant_type: '',
+    client_id: '',
+    client_secret: '',
+    redirect_uri: '',
+    response_type: '',
+    adminGroup: '',
+    api_gateway: ''
+}
