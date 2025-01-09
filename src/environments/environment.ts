bac626841d34e873536c7f54ca1afc23a1d3b2f5
Kjeff24 <jeffreyarthur123@gmail.com>
2025-01-09 13:25:51 +0000

    Added environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    user_pool_domain_uri: process.env['USER_POOL_DOMAIN_URI'],
+    token_endpoint: process.env['TOKEN_ENDPOINT'],
+    login_endpoint: process.env['LOGIN_ENDPOINT'],
+    grant_type: 'authorization_code',
+    client_id: process.env['CLIENT_ID'],
+    client_secret: process.env['CLIENT_SECRET'],
+    redirect_uri: process.env['REDIRECT_URI'],
+    response_type: 'code',
+    adminGroup: process.env['ADMIN_GROUP'],
+    api_gateway: process.env['API_GATEWAY']
+}
