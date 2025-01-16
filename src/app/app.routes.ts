d16b89217b0d497ad94bdf24ba2e5caec17a6b7c
kjeff24 <jeffreyarthur123@gmail.com>
2025-01-16 09:21:11 +0000

    Added users component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { UsersComponent } from './pages/users/users.component';
+import { adminGuard } from './guards/admin/admin.guard';
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
         title: 'Task Management | Home',
+    },
+    {
+        path: 'users',
+        component: UsersComponent,
+        title: 'Task Management | Users',
+        canActivate: [adminGuard]
     },
 ];
